5c3cea34ea2fe895067eae2db3b3cfa45bb3c2c8
Ryan Chau <58543643+killa4nio@users.noreply.github.com>
2020-06-15 03:10:43 +0800

    Update ip-location.js

--- ip-location.js
+++ ip-location.js
@@ -1,9 +1,3 @@
-//原脚本地址 https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/IP_API.js
-
-if ($response.statusCode != 200) {
-    $done(null);
-}
-
 var default_city = "Los Angeles";
 var default_isp = "Cross-GFW.org";
 
@@ -23,17 +17,9 @@ function ISP_ValidCheck(para) {
     }
 }
 
-function Area_check(para) {
-    if (para == "Taiwan") {
-        return "China Taiwan"
-    } else {
-        return para
-    }
-}
-
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = Area_check(obj['country']);
+var title = obj['country'];
 var subtitle = City_ValidCheck(obj['city']) + '-' + ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = obj['isp'] + '\n' + City_ValidCheck(obj['regionName']) + '\n' + obj['query'] + '\n' + obj['timezone'];
