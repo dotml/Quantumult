ba1a4657f93b76e7a2d795e4fa766cee19ab07ac
Ryan Chau <58543643+killa4nio@users.noreply.github.com>
2020-06-15 02:53:26 +0800

    Update ip-location.js

--- ip-location.js
+++ ip-location.js
@@ -1,42 +1,40 @@
 //原脚本地址 https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/IP_API.js
 
 if ($response.statusCode != 200) {
-  $done(null);
+    $done(null);
 }
 
 var default_city = "Los Angeles";
 var default_isp = "Cross-GFW.org";
 
 function City_ValidCheck(para) {
-  if(para) {
-    return para
-  } else
-  {
-    return default_city
-  }
+    if (para) {
+        return para
+    } else {
+        return default_city
+    }
 }
 
 function ISP_ValidCheck(para) {
-  if(para) {
-    return para
-  } else
-  {
-    return default_isp
-  }
+    if (para) {
+        return para
+    } else {
+        return default_isp
+    }
 }
 
 function Area_check(para) {
-  if(para=="Taiwan"){
-    return "China Taiwan"
-  } else
-  {
-    return para
-  }
+    if (para == "Taiwan") {
+        return "China Taiwan"
+    } else {
+        return para
+    }
 }
+
 var body = $response.body;
 var obj = JSON.parse(body);
-var title =Area_check(obj['country']);
-var subtitle =City_ValidCheck(obj['city']) + '-' + '(' + ISP_ValidCheck(obj['org']) + ')';
+var title = Area_check(obj['country']);
+var subtitle = City_ValidCheck(obj['city']) + '-' + ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = obj['isp'] + '\n' + City_ValidCheck(obj['regionName']) + '\n' + obj['query'] + '\n' + obj['timezone'];
 
