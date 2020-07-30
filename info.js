a5e6cdf7b63e7ca5d18f5dd2b56cf90a60fe48b8
Ryan Chau <58543643+killa4nio@users.noreply.github.com>
2020-07-30 22:48:40 +0800

    Update info.js

--- info.js
+++ info.js
@@ -2,30 +2,56 @@ if ($response.statusCode != 200) {
     $done(Null);
 }
 
-var default_city = "Los Angeles";
-var default_isp = "Cross-GFW.org";
+var default_regionName = "Oops!!";
+var default_city = "Oh No!!";
+var default_org = "Cross Utility Ltd";
 
-function City_ValidCheck(para) {
+/**
+ * @return {string}
+ */
+function RegionName_Check(para) {
     if (para) {
-        return para
+        return para;
     } else {
-        return default_city
+        return default_regionName;
     }
 }
 
-function ISP_ValidCheck(para) {
+/**
+ * @return {string}
+ */
+function City_Check(para) {
     if (para) {
-        return para
+        return para;
     } else {
-        return default_isp
+        return default_city;
+    }
+}
+
+/**
+ * @return {string}
+ */
+function Org_Check(para) {
+    if (para) {
+        return para;
+    } else {
+        return default_org;
     }
 }
 
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = City_ValidCheck(obj['city']) + '-' + ISP_ValidCheck(obj['org']);
+var subtitle = default_city + default_org;
+
+var index = obj['org'].indexOf(' (');
+if (index != -1) {
+    subtitle = City_Check(obj['city']) + '-' + Org_Check(obj['org'].substring(0, index));
+} else {
+    subtitle = City_Check(obj['city']) + '-' + Org_Check(obj['org']);
+}
+
 var ip = obj['query'];
-var description = obj['isp'] + '\n' + City_ValidCheck(obj['regionName']) + '\n' + obj['query'] + '\n' + obj['timezone'];
+var description = obj['isp'] + '\n' + RegionName_Check(obj['regionName']) + '\n' + obj['query'] + '\n' + obj['timezone'];
 
 $done({title, subtitle, ip, description});
