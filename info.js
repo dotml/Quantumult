9db61860d9796aa6ff14c62866a6c097519597fe
Ryan Chau <58543643+killa4nio@users.noreply.github.com>
2020-08-12 21:02:17 +0800

    Update info.js

--- info.js
+++ info.js
@@ -52,6 +52,6 @@ if (index != -1) {
 }
 
 var ip = obj['query'];
-var description = obj['country'] + '\n' + RegionName_Check(obj['regionName']) + '\n' + City_Check(obj['city']) + '\n' + obj['timezone'] + '\n' + obj['query'] + '\n' + obj['isp'] + '\n' + Org_Check(obj['org']);
+var description = obj['country'] + '\n' + RegionName_Check(obj['regionName']) + '\n' + City_Check(obj['city']) + '\n' + obj['query'] + '\n' + obj['isp'] + '\n' + Org_Check(obj['org']);
 
 $done({title, subtitle, ip, description});
