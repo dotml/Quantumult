7baead6ea2198970b0d812ef75ed57eb79cfd4f0
Ryan Chau <58543643+killa4nio@users.noreply.github.com>
2020-09-07 06:47:49 +0800

    Update caiyun.js

--- Task/caiyun.js
+++ Task/caiyun.js
@@ -270,7 +270,7 @@ function realtimeWeather() {
 🌡 体感${realtime.life_index.comfort.desc} ${
       realtime.apparent_temperature
     } ℃  💧 湿度 ${(realtime.humidity * 100).toFixed(0)}%
-☂️ 紫外线 ${realtime.life_index.ultraviolet.desc} ☁️ ${mapWind(
+☂️ 紫外线 ${realtime.life_index.ultraviolet.desc} 🪁 ${mapWind(
       realtime.wind.speed,
       realtime.wind.direction
     )}
